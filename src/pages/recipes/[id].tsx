c2cf85f6b441e91c369052a4c76bfbeaf967ef9d
littletake <ritorut18@gmail.com>
2021-03-26 12:47:15 +0900

    feat: add ogp (test)

--- src/pages/recipes/[id].tsx
+++ src/pages/recipes/[id].tsx
@@ -1,4 +1,6 @@
 import type { GetServerSideProps, NextPage } from 'next';
+import Head from 'next/head';
+import { useRouter } from 'next/router';
 import { getRecipe, QueryParameterGetRecipe } from '../../lib/recipe';
 
 import type { Recipe } from '../../types/recipe';
@@ -6,20 +8,31 @@ import { RecipePage } from 'src/components/templates/recipePage';
 import Layout from 'src/components/templates/layout';
 
 type Props = {
+  hostname: string;
   recipe: Recipe;
 };
 
 const EachPage: NextPage<Props> = (props) => {
-  const { recipe } = props;
-
+  const { hostname, recipe } = props;
+  const router = useRouter();
+  const url = hostname + router.asPath;
   if (recipe === null) {
     return <div>loading...</div>;
   }
 
   return (
-    <Layout title="詳細ページ">
-      <RecipePage recipe={recipe} />
-    </Layout>
+    <>
+      <Head>
+        <meta property="og:title" content={recipe.title} />
+        {recipe.image_url && (
+          <meta property="og:image" content={recipe.image_url} />
+        )}
+        <meta property="og:url" content={url} />
+      </Head>
+      <Layout title="詳細ページ">
+        <RecipePage recipe={recipe} />
+      </Layout>
+    </>
   );
 };
 
@@ -36,6 +49,7 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
     const response = await getRecipe(queryParameter);
     return {
       props: {
+        hostname: context.req.headers.host,
         recipe: response.recipes[0],
       },
     };
